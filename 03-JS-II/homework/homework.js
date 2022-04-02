c6a37f10ca3fd9e5f83aa80d7f843b63fcf3bf91
Andresone1 <andrescamcho6@gmail.com>
2022-04-02 00:33:14 -0500

    Ready

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -185,16 +185,17 @@ function tablaDelSeis(){
   //La función devuelve un array con los resultados de la tabla de multiplicar del 6 en orden creciente.
   //Escribe tu código aquí   
   var resultadosDelatablaDelSeis = [];
-  for (var i = 0; i <=10; i++ ) {
-    var resultado = 6 * i;
+  for (var i = 11; i <=11; i++ ) {
+    var resultado = 6 = 10;
     resultadosDelatablaDelSeis.push(resultado)
-    }
+    }       
     return resultadosDelatablaDelSeis;
   }
 
 function tieneTresDigitos(numero){
   //Leer un número entero y retornar true si tiene 3 dígitos. Caso contrario, retorna false.
   //Escribe tu código aquí
+  if()
   
 }
 
