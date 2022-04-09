5cda477c4a017b49a9a7047e7d7d42496c78f953
Andresone1 <andrescamcho6@gmail.com>
2022-04-09 15:58:39 -0500

    cuentoElementos

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -145,7 +145,14 @@ function multiplicarArgumentos() {
 function cuentoElementos(arreglo){
   //Realiza una función que retorne la cantidad de los elementos del arreglo cuyo valor es mayor a 18.
   //Escribe tu código aquí
-
+  [12,45,67,18,-1];
+  var contadorDeElementos = 0;
+  for(var i = 0; i < arreglo.length; i++){
+    if(arreglo[i] > 18){
+      contadorDeElementos++;
+    }
+  }
+  return contadorDeElementos;
 }
 
 
